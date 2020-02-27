06ed398b64f84b01e77d746c8a0544495065082b
xiewj <254584437@qq.com>
2020-02-27 21:12:28 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -185,7 +185,7 @@ export const asyncRoutes = [
   },
 
   /** when your routing map is too long, you can split it into small modules **/
-  componentsRouter,
+  //componentsRouter,
   chartsRouter,
   nestedRouter,
   tableRouter,
